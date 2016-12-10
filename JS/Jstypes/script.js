288cc15c64d8a3ec21f3afc05561a5d2a5955e2b
andreev09 <teman77@gmail.com>
2016-12-10 19:03:51 +0300

    Типы данных

---
+++ JS/Jstypes/script.js
@@ -0,0 +1,14 @@
+var n = 123 //тип - число
+n = 12.123 //тип - число
+
+var t = 'Text' //тип - строка
+
+var hacked = true; //тип - boolean = true
+var hacked = false; //тип - boolean = false
+
+var iknow = null; //тип ничего
+
+var code = {
+  "javascript": true,
+  "ruby": false,
+};
